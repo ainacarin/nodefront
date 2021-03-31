b1dc2e68cfca5b66f6fed20897c2785f4708cb7d
Aida N <nacarinobazaga@gmail.com>
2021-03-31 10:28:26 +0200

    Modified fixed home made management login page with react-fundaments class management

--- src/components/auth/LoginPage/LoginPage.js
+++ src/components/auth/LoginPage/LoginPage.js
@@ -9,9 +9,13 @@ import AuthContext from "../context";
 function LoginPage({ history, location }) {
   const [error, setError] = React.useState(null);
   const [isLoading, setIsLoading] = React.useState(false);
-  // const isLogged = React.useRef(false);
 
-  const { isLogged, onLogin } = React.useContext(AuthContext);
+  const isLogged = React.useRef(false);
+  const firstTime = React.useRef(true);
+
+  // const { isLogged, onLogin } = React.useContext(AuthContext);
+
+  const { onLogin } = React.useContext(AuthContext);
 
   console.log("LoginPage isLogged", isLogged);
   console.log("LoginPage onLogin", onLogin);
@@ -19,22 +23,51 @@ function LoginPage({ history, location }) {
   const resetError = () => setError(null);
 
   // React.useEffect(() => {
-  //   if (isLogged.current) {
-  //     onLogin();
-  //     const { from } = location.state || { from: { pathname: '/' } };
+  //   if (isLogged) {
+  //     // onLogin();
+  //     setIsLoading(false);
+  //   console.log('loading useEffect', isLoading);
+  //     const { from } = location.state || { from: { pathname: "/" } };
   //     history.replace(from);
   //   }
-  // });
+  // }, [isLogged, isLoading, location, history]);
+
+
+  // const handleSubmit = async (credentials, saveSession) => {
+  //   console.log('credentials', credentials);
+  //   console.log('flag', saveSession);
+  //   resetError();
+  //   setIsLoading(true);
+  //   console.log('loading submit', isLoading);
+  //   try {
+  //     await login(credentials, saveSession);
+  //     onLogin();
+  //     // isLogged.current = true;
+  //   } catch (error) {
+  //     setIsLoading(false);
+  //   console.log('loading error', isLoading);
+  //     setError(error);
+  //   } finally {
+  //     // setIsLoading(false);
+  //   }
+  // };
 
   React.useEffect(() => {
-    if (isLogged) {
-      // onLogin();
-      setIsLoading(false);
-    console.log('loading useEffect', isLoading);
-      const { from } = location.state || { from: { pathname: "/" } };
+    if (isLogged.current) {
+      onLogin();
+      const { from } = location.state || { from: { pathname: '/' } };
+      // const from = location.state ? location.state.from : {pathname: '/'}
+
       history.replace(from);
     }
-  }, [isLogged, isLoading, location, history]);
+  });
+
+  React.useEffect(() => {
+    if (firstTime) {
+      // Do things only the first time
+      firstTime.current = false;
+    }
+  });
 
   const handleSubmit = async (credentials, saveSession) => {
     console.log('credentials', credentials);
@@ -44,16 +77,14 @@ function LoginPage({ history, location }) {
     console.log('loading submit', isLoading);
     try {
       await login(credentials, saveSession);
-      onLogin();
-      // isLogged.current = true;
+      isLogged.current = true;
     } catch (error) {
-      setIsLoading(false);
-    console.log('loading error', isLoading);
       setError(error);
     } finally {
-      // setIsLoading(false);
+      setIsLoading(false);
     }
   };
+
   return (
     <div className="loginPage">
       <h1 className="loginPage-title">Accede a Nodepop</h1>
